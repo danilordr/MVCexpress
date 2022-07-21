335f996de2a17e341847126f02b65ff92b4c683b
danilordr <danilobecao@gmail.com>
2022-07-21 15:37:25 -0300

    subindo

--- app.js
+++ app.js
@@ -7,19 +7,27 @@ const app = express();
 // definindo uma rota de acesso ao sistema
 //parametro 1: caminho ou url  da rota 
 // parametro 2: callback a ser execultado toda vez que acessamos a rota 
-app.get('/',(req,res)=>{
+app.get('/',(req, res) => {
   return res.send('Olá turma!');
 });
 
 const usuarios = require('./models/usuarios');
-const usuariosController = require('./controllers/usuarios');
-app.get('/usuario/:id',usuariosController.get);
+app.get('/usuarios/:id', (req, res) => {
+  const { id } = req.params; // Entrada de dados!
+  const dados = usuarios.filter((item) => item.id == id); // Chamada para o modelo (busca info.)
+  return res.send(dados[0].nome + ' ' + dados[0].idade); // Saída de dados!
+});
 //criaçao rota mvc
-
+const usuariosController = require('./controllers/usuarios');
+app.get('/usuario/:id', usuariosController.get);
+//adicionando tramplete engine
+app.set('view engine' , 'ejs');
+//criar local arquivos estatico
+app.use(express.static('public'));
 // Disponibilizamos uma porta para que nosso
 // computador possa receber mensagens através dela
 //acessar no no navegador http://localhost:3000/
 //precisa estar por ultimo no arquivo
-app.listen(3000 ,() =>{
+app.listen(3000 ,() => {
   console.log('servidor rodando na porta 3000 http://localhost:3000/')
 });
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -3,10 +3,11 @@ const usuarios= require('../models/usuarios')
 // as informaçao adicional e capturada atraves do cparmaetro da url representado pela sintaxe id
 // localhost:3000/usuario/1, o numero 1 sera armezenado na variavel id
 
-const usuariosController = { get: (req,res)=>{
+const usuariosController = { 
+  get: (req,res) => {
     const { id } = req.params;//entrada de dados
-  const dados= usuarios.filter((usuario)=> usuario.id == id);//chamadapara o modelo(buscar info.)
-  return res.send(dados[0].nome + ' '+ dados[0].idade);//saida de dados
+  const dados = usuarios.filter((item) => item.id == id);//chamadapara o modelo(buscar info.)
+  return res.render('user',{dados});//saida de dados para a view (template engine)
   }
 
 }
